5d0851b7cb252af59a5f8d80d3ef048aa002fb36
Shubham Bankar <103604945+ignalpha4@users.noreply.github.com>
2024-11-28 11:24:40 +0530

    fix: minor text fixes

--- JEEVAN/js/main.js
+++ JEEVAN/js/main.js
@@ -2,9 +2,6 @@
 (function ($) {
     "use strict";
 
-    
-    /*==================================================================
-    [ Validate ]*/
     var input = $('.validate-input .input100');
 
     $('.validate-form').on('submit',function(){
@@ -51,7 +48,6 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
     
 
 })(jQuery);
